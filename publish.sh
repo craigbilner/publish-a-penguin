28cb1048b6cf86c54d55deab4d0b146cc6b00f42
Craig Bilner <craig.bilner@news.co.uk>
2017-08-10 14:41:05 +0100

    fix: set up git

--- publish.sh
+++ publish.sh
@@ -8,17 +8,18 @@ then
   echo "Setting up npm"
   echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
+  # set up git
+  git config user.name "Publish Bot"
+  git config user.email "publish@ghbot.com"
+
+  git remote set-url origin https://${GH_TOKEN}@github.com/craigbilner/publish-a-penguin.git > /dev/null 2>&1
+
   # bump versions, create change logs, create tags, publish to npm
   MESSAGE=$(printf "chore: Publish %s" $TRAVIS_PULL_REQUEST_BRANCH)
   echo $MESSAGE
   lerna publish --conventional-commits --yes --concurrency=1 --exact -m $MESSAGE
 
   # push above changes to git
-  git config user.name "Publish Bot"
-  git config user.email "publish@ghbot.com"
-
-  git remote set-url origin https://${GH_TOKEN}@github.com/craigbilner/publish-a-penguin.git > /dev/null 2>&1
-
   echo "Pushing to master"
   git push origin master --tags --quiet > /dev/null 2>&1
 else
