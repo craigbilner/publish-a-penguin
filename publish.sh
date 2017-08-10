57dbb2361b4b15a7188bfa985bf2a3e8f22daa2d
Craig Bilner <craig.bilner@news.co.uk>
2017-08-10 18:08:39 +0100

    chore: add logging

--- publish.sh
+++ publish.sh
@@ -3,6 +3,21 @@ set -e
 
 echo $(printf "Running publish script tb: %s, tt: %s, tpr: %s" $TRAVIS_BRANCH $TRAVIS_TAG $TRAVIS_PULL_REQUEST)
 
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_TAG = 'false' && -z $TRAVIS_PULL_REQUEST ]]
+then
+  echo "yes! 1"
+fi
+
+if [[ $TRAVIS_BRANCH == 'master' && -z $TRAVIS_TAG && -z $TRAVIS_PULL_REQUEST ]]
+then
+  echo "yes! 2"
+fi
+
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_TAG == 'false' && -z $TRAVIS_PULL_REQUEST ]]
+then
+  echo "yes! 3"
+fi
+
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_TAG = '' && -z $TRAVIS_PULL_REQUEST ]]
 then
   # set npm credentials
